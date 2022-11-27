eedac520e1ea22dfcb6a2eef2cab9b7c450607bf
Pedro E <55996330+desertpetrol@users.noreply.github.com>
2022-11-27 16:09:18 -0300

    Delete java directory

--- java/Gerador de email/src/emailapp/EmailApp.java
+++
@@ -1,9 +0,0 @@
-package emailapp;
-
-public class EmailApp {
-    public static void main(String[] args){
-        Email em1 = new Email("José","Santos");
-        System.out.println(em1.showInfo()); 
-    }
-
-}
